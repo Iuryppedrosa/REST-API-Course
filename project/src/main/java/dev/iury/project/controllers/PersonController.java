c498e72524b0ff5d17f594553187d58c6ac85cdc
iurypedrosa <iuryppedrosa@protonmail.com>
2024-04-25 09:48:29 -0300

    Aplicando content type YAML no POST GET e PUT, utilizando uma classe própria no pacote UTIL. Bem como content negotiation em XML e JSON padrao. Usamos a classe YamlJackson2HttpMesageConverter para converter para x-yaml.

--- project/src/main/java/dev/iury/project/controllers/PersonController.java
+++ project/src/main/java/dev/iury/project/controllers/PersonController.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 import dev.iury.project.dataVO.PersonVO;
 import dev.iury.project.dataVO2.PersonVOV2;
 import dev.iury.project.service.PersonService;
+import dev.iury.project.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static dev.iury.project.util.MediaType.APPLICATION_YML;
+
 @RestController
 @RequestMapping("api/person/v1")
 public class PersonController {
@@ -23,44 +25,52 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON,
+                            MediaType.APPLICATION_XML,
+                            MediaType.APPLICATION_YML})
     public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML})
     public Optional<PersonVO> findById(@PathVariable(value = "id") Long id) {
         return Optional.ofNullable(service.findById(id));
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE},
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON,
+                            MediaType.APPLICATION_XML,
+                            MediaType.APPLICATION_YML},
 
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PostMapping(value = "/v2",
-            consumes = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML},
 
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML})
     public PersonVOV2 createv2(@RequestBody PersonVOV2 person) {
         return service.createv2(person);
     }
 
     @PutMapping(value = "/{id}",
-            consumes = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML},
 
-            produces = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON,
+                        MediaType.APPLICATION_XML,
+                        MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person, @PathVariable Long id){
         return service.update(person, id);
     }
---
+++ project/src/main/java/dev/iury/project/util/MediaType.java
@@ -0,0 +1,7 @@
+package dev.iury.project.util;
+
+public class MediaType {
+    public static final String APPLICATION_YML = "application/x-yaml";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_JSON = "application/json";
+}
