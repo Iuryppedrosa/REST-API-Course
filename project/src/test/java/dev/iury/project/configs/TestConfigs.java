fa48cd5b03065f178a19530eecc115eebf2f6217
iurypedrosa <iuryppedrosa@protonmail.com>
2024-05-28 12:47:26 -0300

    Aplicacao configurada e rodando no docker.

--- project/src/test/java/dev/iury/project/configs/TestConfigs.java
+++ project/src/test/java/dev/iury/project/configs/TestConfigs.java
@@ -10,6 +10,4 @@ public class TestConfigs {
     public static final String CONTENT_TYPE_XML = "application/xml";
     public static final String CONTENT_TYPE_YML = "application/x-yaml";
 
-    public static final String ORIGIN_ERUDIO = "https://erudio.com.br";
-    public static final String ORIGIN_SEMERU = "https://semeru.com.br";
 }
